95be9cd7185656415b3ad7cfa93374d6aee6ca47
Plesz Gábor <gabor.plesz@gmail.com>
2018-06-19 19:29:54 +0200

    #9: IDisposable integrálása a repoba

--- AdHr/AdHr/Profiles/AdUserProfile.cs
+++ AdHr/AdHr/Profiles/AdUserProfile.cs
@@ -14,6 +14,7 @@ namespace AdHr.Profiles
         public AdUserProfile()
         {
             CreateMap<AdhrUser, ReadLdapUserResponse>()
+                .ForMember(d=>d.UserCn, o=>o.Ignore()) 
                 ;
 
             CreateMap<AdhrUser, ResponseBase<ReadLdapUserResponse>>()
--- AdHr/AdHr/Repository/AdRepository.cs
+++ AdHr/AdHr/Repository/AdRepository.cs
@@ -10,25 +10,59 @@ using System.Linq.Expressions;
 using AutoMapper;
 using AdHr.Profiles;
 using System.Collections.ObjectModel;
+using System.Threading;
+using System.Security.Principal;
 
 namespace AdHr.Repository
 {
-    public class AdRepository
+    public class AdRepository : IDisposable
     {
-        private readonly PrincipalContext adContext;
-        private readonly IMapper mapper;
+        //mivel ez IDisposable, ezért nekünk is annak kell lennünk
+        private readonly PrincipalContext adContext = null;
+        private IMapper mapper;
+
+        //jelzi, hogy lefutott-e már a Dispose
+        private int IsDisposed=0;
+
+        public AdRepository()
+        {
+            adContext = SetContext();
+            InitializeAutoMapper();
+        }
+
+        public AdRepository(string address)
+        {
+            adContext = SetContext(address);
+            InitializeAutoMapper();
+        }
 
         public AdRepository(string address, string userName, string password)
         {
-            //todo: IDisposable
-            adContext = new PrincipalContext(ContextType.Domain, address, userName, password);
-            //todo: IDisposable
+            adContext = SetContext(address, userName, password);
+            InitializeAutoMapper();
+        }
 
-            //todo: áttenni a lényeget az AdProfile-ba
+        //todo: factory
+        private PrincipalContext SetContext()
+        {
+            return new PrincipalContext(ContextType.Domain);
+        }
+
+        private PrincipalContext SetContext(string address)
+        {
+            return new PrincipalContext(ContextType.Domain, address);
+        }
+
+        private PrincipalContext SetContext(string address, string userName, string password)
+        {
+            return new PrincipalContext(ContextType.Domain, address, userName, password);
+        }
+
+        private void InitializeAutoMapper()
+        {
             var config = new MapperConfiguration(cfg => cfg.AddProfile<AdUserProfile>());
             mapper = config.CreateMapper();
         }
-
 
         public ResponseBase<CreateLdapUserResponse> Create(string login, string password, string description, string displayName)
         {
@@ -107,32 +141,93 @@ namespace AdHr.Repository
 
         private static AdhrUser GetUserInfo(Principal up)
         {
-            var o = (DirectoryEntry)up.GetUnderlyingObject();
-
-            var user = new AdhrUser
+            using (var o = (DirectoryEntry)up.GetUnderlyingObject())
             {
-                DisplayName = up.DisplayName,
-                Description = up.Description,
-                DistinguishedName = up.DistinguishedName,
-                Name = up.Name,
-                Sid = up.Sid,
-                UserPrincipalName = up.UserPrincipalName,
-                SamAccountName = up.SamAccountName,
-            };
 
-            var fields = o.Properties;
-
-            foreach (string fieldName in fields.PropertyNames)
-            {
-                var list = new List<AdhrValue>();
-                foreach (var item in fields[fieldName])
+                var user = new AdhrUser
                 {
-                    list.Add(new AdhrValue(item.ToString()));
+                    DisplayName = up.DisplayName,
+                    Description = up.Description,
+                    DistinguishedName = up.DistinguishedName,
+                    Name = up.Name,
+                    Sid = up.Sid,
+                    UserPrincipalName = up.UserPrincipalName,
+                    SamAccountName = up.SamAccountName,
+                };
+
+                var fields = o.Properties;
+
+                foreach (string fieldName in fields.PropertyNames)
+                {
+                    var list = new List<AdhrValue>();
+                    foreach (var item in fields[fieldName])
+                    {
+                        list.Add(new AdhrValue(item.ToString()));
+                    }
+                    user.Properties.Add(fieldName, new ReadOnlyCollection<AdhrValue>(list));
                 }
-                user.Properties.Add(fieldName, new ReadOnlyCollection<AdhrValue>(list));
+
+                //jogosultság lista lekérése
+                //o.RefreshCache(new string[] { "allowedAttributesEffective" });
+
+                //var writeRigths0 = user.Properties
+                //                      .Select(x => x.Key)
+                //                      .Where(property => o.Properties["allowedAttributesEffective"].Contains(property));
+
+                o.RefreshCache();
+
+                var writeRigths = o.Properties["allowedAttributesEffective"].Value;
+
+                var acl = o.ObjectSecurity;
+                foreach (ActiveDirectoryAccessRule ace in acl.GetAccessRules(true, true, typeof(NTAccount)))
+                {
+                    if (ace.AccessControlType==System.Security.AccessControl.AccessControlType.Allow
+                        && ace.ActiveDirectoryRights == ActiveDirectoryRights.WriteProperty)
+                    {
+                        var ir=ace.IdentityReference.ToString();
+                    }
+                }
+
+                var masks = o.Options.SecurityMasks;
+
+                //var search = new DirectorySearcher(o);
+                //search.PropertiesToLoad.Add("telephoneNumber");
+                //search.PropertiesToLoad.Add("mail");
+                //search.PageSize = 10;
+
+                return user;
+            }
+        }
+
+        #region IDisposable implementation
+        ~AdRepository()
+        {
+            Dispose(false);
+        }
+
+        private void Dispose(bool isDispose)
+        {
+            if (Interlocked.Exchange(ref IsDisposed, 1) == 1)
+            {
+                throw new ObjectDisposedException(nameof(AdRepository));
             }
 
-            return user;
+            if (isDispose)
+            {
+                if (adContext!=null)
+                {
+                    adContext.Dispose();
+                    //todo: mivel ez readonly, nem tudom lenullázni
+                    //adContext = null;
+                }
+            }
         }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion IDisposable implementation
     }
 }
